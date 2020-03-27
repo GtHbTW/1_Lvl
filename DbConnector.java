e1589e64634949dbd15b0170cbb196ba6cee9783
GtHbTW <48619944+GtHbTW@users.noreply.github.com>
2020-03-27 17:48:09 +0100

    Create DbConnector.java

---
+++ DbConnector.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnector {
+    private static String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static String USER = "postgres";
+    private static String PASSWORD = "WSB2020";
+
+    public static Connection connect() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Połączono z serwisem banku ...");
+        }   catch (SQLException e2){
+            e2.printStackTrace();
+        }
+        return connection;
+    }
+}
